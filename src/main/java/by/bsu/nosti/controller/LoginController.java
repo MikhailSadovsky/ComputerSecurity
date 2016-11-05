5585e7f99bef30042bbacf0f7d37812a4ed5114e
Mikhail_Sadovsky <mishachess@mail.ru>
2016-11-05 21:51:24 +0300

    Initial commit

---
+++ src/main/java/by/bsu/nosti/controller/LoginController.java
@@ -0,0 +1,85 @@
+package by.bsu.nosti.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import by.bsu.nosti.entity.User;
+import by.bsu.nosti.entity.UserModel;
+import by.bsu.nosti.entity.UserRole;
+import by.bsu.nosti.exception.DAOException;
+
+@Controller
+public class LoginController extends BaseController {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView getLogin() {
+		return new ModelAndView("login", "user", new User());
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String getLogout(HttpServletRequest req, HttpServletResponse resp, Model model) {
+		removeCookie("login", req, resp);
+		return "index";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String processLogin(@ModelAttribute("user") User user, BindingResult result, HttpServletRequest req,
+			HttpServletResponse resp, Model model) {
+		try {
+			User userDb = userDAO.getUser(user.getLogin(), user.getPassword());
+			if (userDb == null) {
+				result.rejectValue("email", "registration.email.wrongPattern", "Login or password is incorrect!");
+			}
+			if (result.hasErrors()) {
+				return "login";
+			} else {
+				addCookie(userDb.getLogin(), resp);
+			}
+			model.addAttribute("currentuser", userDb.getLogin());
+			if (userDb != null && userDb.getRole() == UserRole.Admin.ordinal()) {
+				model.addAttribute("userResourceLinks", userResourceLinkDAO.retrieveAll());
+				return "admin";
+			}
+			model.addAttribute("userResourceLinks",
+					userResourceLinkDAO.getUserResourceLinkByUserId(userDb.getUserId()));
+			return "index";
+		} catch (DAOException exception) {
+			return "error";
+		}
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.GET)
+	public ModelAndView processRegister() {
+		return new ModelAndView("registration", "person", new UserModel());
+
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public String processSubmit(@ModelAttribute("person") UserModel person, HttpServletRequest req,
+			HttpServletResponse resp, Model model) {
+		try {
+			User user = new User();
+			user.setEmail(person.getEmail());
+			user.setLogin(person.getLogin());
+			user.setPassword(person.getPassword());
+			user.setRole(UserRole.User.ordinal());
+			userDAO.create(user);
+			model.addAttribute("currentuser", user.getLogin());
+			if (user != null && user.getRole() == UserRole.Admin.ordinal()) {
+				model.addAttribute("userResourceLinks", userResourceLinkDAO.retrieveAll());
+				return "admin";
+			}
+			model.addAttribute("userResourceLinks", userResourceLinkDAO.getUserResourceLinkByUserId(user.getUserId()));
+			return "index";
+		} catch (DAOException exception) {
+			return "error";
+		}
+	}
+}
